6c4c92f6884806d187bc0bf914041e7288f63c3f
Stefan Mitterrutzner <stefan.mitterrutzner@gmail.com>
2020-04-17 08:53:06 +0200

    exclude database from backup

---
+++ Sources/DP3TSDK/utils/URL+Backup.swift
@@ -0,0 +1,18 @@
+/*
+ * Created by Ubique Innovation AG
+ * https://www.ubique.ch
+ * Copyright (c) 2020. All rights reserved.
+ */
+
+import Foundation
+
+
+extension URL {
+    /// adds the isExcludedFromBackup Attribute to a fileURL
+    /// - Throws: if a error occured while setting the attribute
+    mutating func addExcludedFromBackupAttribute() throws {
+        var resourceValues = URLResourceValues()
+        resourceValues.isExcludedFromBackup = true
+        try self.setResourceValues(resourceValues)
+    }
+}
