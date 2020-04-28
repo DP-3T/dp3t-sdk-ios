73bcdc016f4d3e1c7fafe724d2fef51b4be6c60a
Stefan Mitterrutzner <stefan.mitterrutzner@gmail.com>
2020-04-28 11:17:38 +0200

    fixes unit tests

--- Tests/DP3TSDKTests/ContactFactoryTests.swift
+++ Tests/DP3TSDKTests/ContactFactoryTests.swift
@@ -10,13 +10,17 @@ import XCTest
 final class ContactFactoryTests: XCTestCase {
     func testShouldGrouping() {
         let token = Data(base64Encoded: "30+i6bJjzmOWMa0uUPH9LA==")!
-        let date = DP3TCryptoModule.getEpochStart().addingTimeInterval(.minute * 30)
+        let epochStart = DP3TCryptoModule.getEpochStart()
+        let date = epochStart.addingTimeInterval(.minute * 30)
         let handshake1 = HandshakeModel(identifier: 0, timestamp: date, ephID: token, TXPowerlevel: nil, RSSI: -30)
         let handshake2 = HandshakeModel(identifier: 1, timestamp: date.addingTimeInterval(1), ephID: token, TXPowerlevel: nil, RSSI: -30)
         let handshakes = [handshake1, handshake2]
         let contacts = ContactFactory.contacts(from: handshakes)
+
+        let bucketStart = date.timeIntervalSince1970 - date.timeIntervalSince1970.truncatingRemainder(dividingBy: NetworkingConstants.batchLength)
+
         XCTAssertEqual(contacts.count, 1)
-        XCTAssertEqual(contacts.first?.day , DayDate(date: date))
+        XCTAssertEqual(contacts.first?.date , Date(timeIntervalSince1970: bucketStart))
         XCTAssertEqual(contacts.first?.ephID, token)
     }
 
