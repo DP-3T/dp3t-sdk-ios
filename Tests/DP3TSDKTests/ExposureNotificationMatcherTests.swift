4dba5f7efbad4155e1d68a27ae00e8a5e0f8456c
Stefan Mitterrutzner <stefan.mitterrutzner@gmail.com>
2020-05-21 09:01:32 +0200

    adds ExposureNotificationMatcherTests

--- Tests/DP3TSDKTests/ExposureNotificationMatcherTests.swift
+++ Tests/DP3TSDKTests/ExposureNotificationMatcherTests.swift
@@ -10,37 +10,60 @@ import Foundation
 import ExposureNotification
 import Foundation
 import XCTest
+import ZIPFoundation
 
-/*
+fileprivate class MockSummary: ENExposureDetectionSummary {
+    override var attenuationDurations: [NSNumber] {
+        get {
+            internalAttenutationDurations
+        }
+        set {
+            internalAttenutationDurations = newValue
+        }
+    }
+    private var internalAttenutationDurations: [NSNumber] = []
+}
 
- fileprivate class MockManager: ENManager {
+fileprivate class MockManager: ENManager {
 
- var detectExposuresWasCalled = false
+    var detectExposuresWasCalled = false
 
- override func detectExposures(configuration: ENExposureConfiguration, diagnosisKeyURLs: [URL], completionHandler: @escaping ENDetectExposuresHandler) -> Progress {
-     detectExposuresWasCalled = true
-     let summary = ENExposureDetectionSummary()
-     summary.attenuationDurations = [1800,1800]
-     completionHandler(summary,nil)
-     return Progress()
- }
+    var data: [Data] = []
 
- }
+    override func detectExposures(configuration: ENExposureConfiguration, diagnosisKeyURLs: [URL], completionHandler: @escaping ENDetectExposuresHandler) -> Progress {
+        detectExposuresWasCalled = true
+        let summary = MockSummary()
+        summary.attenuationDurations = [0,0,0]
+        completionHandler(summary,nil)
+        diagnosisKeyURLs.forEach{
+            let diagData = try! Data(contentsOf: $0)
+            data.append(diagData)
+        }
+        return Progress()
+    }
 
- final class ExposureNotificationMatcherTests: XCTestCase {
- var keychain = MockKeychain()
+}
 
- override func tearDown() {
-     keychain.reset()
- }
+final class ExposureNotificationMatcherTests: XCTestCase {
+    var keychain = MockKeychain()
 
- func testCallingOfMatcher(){
-     let mockmanager = MockManager()
-     let storage = ExposureDayStorage(keychain: keychain)
-     let matcher = ExposureNotificationMatcher(manager: mockmanager, exposureDayStorage: storage)
-     try! matcher.receivedNewKnownCaseData("data1".data(using: .utf8)!, batchTimestamp: Date())
-     try! matcher.finalizeMatchingSession()
-     XCTAssert(mockmanager.detectExposuresWasCalled)
- }
- }
- */
+    override func tearDown() {
+        keychain.reset()
+    }
+
+    func testCallingOfMatcher(){
+        let mockmanager = MockManager()
+        let storage = ExposureDayStorage(keychain: keychain)
+        let matcher = ExposureNotificationMatcher(manager: mockmanager, exposureDayStorage: storage)
+
+        let data = "Some string!".data(using: .utf8)!
+        guard let archive = Archive(accessMode: .create) else { return }
+        try! archive.addEntry(with: "inMemory.bin", type: .file, uncompressedSize: 12, bufferSize: 4, provider: { (position, size) -> Data in
+            return data.subdata(in: position..<position+size)
+        })
+        try! matcher.receivedNewKnownCaseData(archive.data!, keyDate: Date())
+        try! matcher.finalizeMatchingSession()
+        XCTAssert(mockmanager.detectExposuresWasCalled)
+        XCTAssert(mockmanager.data.contains(data))
+    }
+}
