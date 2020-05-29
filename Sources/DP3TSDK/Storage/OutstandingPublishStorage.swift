2046bdd058a6c7ccf4410494dd2ca43be03b1faf
Stefan Mitterrutzner <stefan.mitterrutzner@gmail.com>
2020-05-29 11:04:00 +0200

    adds logs to OutstandingPublishStorage

--- Sources/DP3TSDK/Storage/OutstandingPublishStorage.swift
+++ Sources/DP3TSDK/Storage/OutstandingPublishStorage.swift
@@ -9,6 +9,8 @@ import Foundation
 class OutstandingPublishStorage {
     let keychain: KeychainProtocol
 
+    private let logger = Logger(OutstandingPublishOperation.self, category: "OutstandingPublishStorage")
+
     static let key = KeychainKey<[OutstandingPublish]>(key: "org.dpppt.outstandingpublish")
 
     init(keychain: KeychainProtocol = Keychain()) {
@@ -19,18 +21,21 @@ class OutstandingPublishStorage {
         switch keychain.get(for: Self.key) {
         case let .success(publishes):
             return publishes
-        case .failure:
+        case let .failure(error):
+            logger.error("could not access keychaing error: %{public}@", error.localizedDescription)
             return []
         }
     }
 
     func add(_ publish: OutstandingPublish) {
+        logger.log("adding publish operation for %{public}@", publish.dayToPublish.debugDescription)
         var elements = get()
         elements.append(publish)
         set(publishes: elements)
     }
 
     func remove(publish: OutstandingPublish) {
+        logger.log("removing publish operation for %{public}@", publish.dayToPublish.debugDescription)
         var elements = Set(get())
         elements.remove(publish)
         set(publishes: Array(elements))
