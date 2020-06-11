7aa7f27fdef5df38e0081383d147cfb9c4e259c9
Stefan Mitterrutzner <stefan.mitterrutzner@gmail.com>
2020-06-11 18:11:57 +0200

    removed logging storage from sdk

--- Package.swift
+++ Package.swift
@@ -12,12 +12,9 @@ let package = Package(
             name: "DP3TSDK",
             targets: ["DP3TSDK"]
         ),
-        .library(name: "DP3TSDK_LOGGING_STORAGE",
-                 targets: ["DP3TSDK_LOGGING_STORAGE"]),
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.6.0"),
-        .package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.12.0"),
         .package(url: "https://github.com/IBM-Swift/Swift-JWT.git", from: "3.6.1"),
         .package(url: "https://github.com/weichsel/ZIPFoundation/", .upToNextMajor(from: "0.9.0")),
     ],
@@ -25,10 +22,6 @@ let package = Package(
         .target(
             name: "DP3TSDK",
             dependencies: ["SwiftJWT", "ZIPFoundation"]
-        ),
-        .target(
-            name: "DP3TSDK_LOGGING_STORAGE",
-            dependencies: ["SQLite"]
         ),
         .testTarget(
             name: "DP3TSDKTests",
