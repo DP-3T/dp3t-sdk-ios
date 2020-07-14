637dcac55bc50ed27c42832da17935099296c542
Stefan Mitterrutzner <stefan.mitterrutzner@gmail.com>
2020-07-14 10:56:33 +0200

    if activation or enable failed for some reason retry on willEnterForeground

--- Sources/DP3TSDK/Tracing/ExposureNotificationTracer.swift
+++ Sources/DP3TSDK/Tracing/ExposureNotificationTracer.swift
@@ -28,6 +28,10 @@ class ExposureNotificationTracer: Tracer {
 
     private let managerClass: ENManager.Type
 
+    private var isActivated: Bool = false
+
+    private var deferredActivation: Bool = false
+
     private(set) var state: TrackingState {
         didSet {
             guard oldValue != state else { return }
@@ -42,14 +46,30 @@ class ExposureNotificationTracer: Tracer {
 
         state = .initialization
 
+        activateManager()
+
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(willEnterForeground),
+                                               name: UIApplication.willEnterForegroundNotification,
+                                               object: nil)
+    }
+
+    func activateManager(){
         logger.log("calling ENMananger.activate")
         manager.activate { [weak self] error in
             guard let self = self else { return }
-            self.queue.async {
+            self.queue.sync {
                 if let error = error {
                     self.logger.error("ENMananger.activate failed error: %{public}@", error.localizedDescription)
+                    self.state = .inactive(error: .exposureNotificationError(error: error))
                 } else {
+                    self.isActivated = true
                     self.initializeObservers()
+                    
+                    if self.deferredActivation,
+                        TrackingState.active != self.state{
+                        self.setEnabled(self.deferredActivation, completionHandler: nil)
+                    }
                 }
                 self.logger.log("notify callbacks after initialisation (count: %d)", self.initializationCallbacks.count)
                 self.initializationCallbacks.forEach {
@@ -58,10 +78,6 @@ class ExposureNotificationTracer: Tracer {
                 self.initializationCallbacks.removeAll()
             }
         }
-        NotificationCenter.default.addObserver(self,
-                                               selector: #selector(willEnterForeground),
-                                               name: UIApplication.willEnterForegroundNotification,
-                                               object: nil)
     }
 
     func addInitialisationCallback(callback: @escaping  ()-> Void ){
@@ -83,7 +99,18 @@ class ExposureNotificationTracer: Tracer {
     var isAuthorized: Bool { ENManager.authorizationStatus == .authorized }
 
     @objc func willEnterForeground(){
-        updateState()
+        self.queue.sync {
+            if !self.isActivated {
+                self.activateManager()
+            }
+
+            if self.deferredActivation,
+                TrackingState.active != self.state{
+                self.setEnabled(self.deferredActivation, completionHandler: nil)
+            } else {
+                self.updateState()
+            }
+        }
     }
 
     func initializeObservers() {
@@ -99,21 +126,40 @@ class ExposureNotificationTracer: Tracer {
     }
 
     func updateState() {
-        state = .init(state: manager.exposureNotificationStatus,
-                      authorizationStatus: managerClass.authorizationStatus,
-                      enabled: manager.exposureNotificationEnabled)
+        guard self.isActivated else { return }
+
+        self.state = .init(state: self.manager.exposureNotificationStatus,
+                           authorizationStatus: self.managerClass.authorizationStatus,
+                           enabled: self.manager.exposureNotificationEnabled)
     }
 
     func setEnabled(_ enabled: Bool, completionHandler: ((Error?) -> Void)?) {
         logger.log("calling ENMananger.setExposureNotificationEnabled %{public}@", enabled ? "true" : "false")
 
+        guard self.isActivated else {
+            logger.log("could not activate since manager is not activated")
+            self.deferredActivation = enabled
+            
+            // use stored error if available
+            if case let TrackingState.inactive(error: error) = state {
+                completionHandler?(error)
+            } else {
+                completionHandler?(DP3TTracingError.permissonError)
+            }
+            return
+        }
+
+        if !enabled { deferredActivation = false }
+
         manager.setExposureNotificationEnabled(enabled) { [weak self] error in
             guard let self = self else { return }
             if let error = error {
                 self.logger.error("ENMananger.setExposureNotificationEnabled failed error: %{public}@", error.localizedDescription)
+                self.deferredActivation = enabled
                 self.state = .inactive(error: .exposureNotificationError(error: error))
+            } else {
+                self.updateState()
             }
-            self.updateState()
             completionHandler?(error)
         }
     }
@@ -121,11 +167,6 @@ class ExposureNotificationTracer: Tracer {
 
 extension TrackingState {
     init(state: ENStatus, authorizationStatus: ENAuthorizationStatus, enabled: Bool) {
-        if authorizationStatus == .unknown {
-            self = .stopped
-            return
-        }
-
         guard authorizationStatus == .authorized else {
             self = .inactive(error: .permissonError)
             return
