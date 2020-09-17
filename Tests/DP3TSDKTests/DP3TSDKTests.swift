9c2c41dd7f1cdc4a0fa136bc45ced5becf246a0c
Stefan Mitterrutzner <stefan.mitterrutzner@gmail.com>
2020-09-17 14:56:25 +0200

    makes tests more robust by moving sdk variables into local scope

--- Tests/DP3TSDKTests/DP3TSDKTests.swift
+++ Tests/DP3TSDKTests/DP3TSDKTests.swift
@@ -27,55 +27,50 @@ private class MockKeyProvider: DiagnosisKeysProvider {
 
 
 class DP3TSDKTests: XCTestCase {
-
-    fileprivate var keychain: MockKeychain!
-    fileprivate var tracer: ExposureNotificationTracer!
-    fileprivate var matcher: ExposureNotificationMatcher!
-    fileprivate var service: MockService!
-    fileprivate var defaults: MockDefaults!
-    fileprivate var keyProvider: MockKeyProvider!
-
-    var descriptor: ApplicationDescriptor {
-        MockService.descriptor
-    }
-
-    fileprivate var backgroundTaskManager: DP3TBackgroundTaskManager!
-    fileprivate var manager: MockENManager!
-    fileprivate var exposureDayStorage: ExposureDayStorage!
-    fileprivate var outstandingPublishStorage: OutstandingPublishStorage!
-    fileprivate var sdk: DP3TSDK!
-
-    override func setUp() {
-        keychain = MockKeychain()
-
-        manager = MockENManager()
-
-        exposureDayStorage = ExposureDayStorage(keychain: keychain)
-
-        defaults = MockDefaults()
-        tracer = ExposureNotificationTracer(manager: manager, managerClass: MockENManager.self)
-        matcher = ExposureNotificationMatcher(manager: manager, exposureDayStorage: exposureDayStorage, defaults: defaults)
-
-        service = MockService()
-        keyProvider = MockKeyProvider()
-        backgroundTaskManager = DP3TBackgroundTaskManager(handler: nil, keyProvider: keyProvider, serviceClient: service, tracer: tracer)
-        outstandingPublishStorage = OutstandingPublishStorage(keychain: keychain)
-        sdk = DP3TSDK(applicationDescriptor: descriptor,
-                          urlSession: MockSession(data: nil, urlResponse: nil, error: nil),
-                          tracer: tracer,
-                          matcher: matcher,
-                          diagnosisKeysProvider: keyProvider,
-                          exposureDayStorage: exposureDayStorage,
-                          outstandingPublishesStorage: outstandingPublishStorage,
-                          service: service,
-                          synchronizer: KnownCasesSynchronizer(matcher: matcher, service: service, defaults: defaults, descriptor: descriptor),
-                          backgroundTaskManager: backgroundTaskManager,
-                          defaults: defaults)
+    fileprivate struct SDK {
+        fileprivate var keychain: MockKeychain!
+        fileprivate var tracer: ExposureNotificationTracer!
+        fileprivate var matcher: ExposureNotificationMatcher!
+        fileprivate var service: MockService!
+        fileprivate var defaults: MockDefaults!
+        fileprivate var keyProvider: MockKeyProvider!
+        var descriptor: ApplicationDescriptor {
+            MockService.descriptor
+        }
+        fileprivate var backgroundTaskManager: DP3TBackgroundTaskManager!
+        fileprivate var manager: MockENManager!
+        fileprivate var exposureDayStorage: ExposureDayStorage!
+        fileprivate var outstandingPublishStorage: OutstandingPublishStorage!
+        fileprivate var sdk: DP3TSDK!
+        init() {
+            keychain = MockKeychain()
+            manager = MockENManager()
+            exposureDayStorage = ExposureDayStorage(keychain: keychain)
+            defaults = MockDefaults()
+            tracer = ExposureNotificationTracer(manager: manager, managerClass: MockENManager.self)
+            matcher = ExposureNotificationMatcher(manager: manager, exposureDayStorage: exposureDayStorage, defaults: defaults)
+            service = MockService()
+            keyProvider = MockKeyProvider()
+            backgroundTaskManager = DP3TBackgroundTaskManager(handler: nil, keyProvider: keyProvider, serviceClient: service, tracer: tracer)
+            outstandingPublishStorage = OutstandingPublishStorage(keychain: keychain)
+            sdk = DP3TSDK(applicationDescriptor: descriptor,
+                              urlSession: MockSession(data: nil, urlResponse: nil, error: nil),
+                              tracer: tracer,
+                              matcher: matcher,
+                              diagnosisKeysProvider: keyProvider,
+                              exposureDayStorage: exposureDayStorage,
+                              outstandingPublishesStorage: outstandingPublishStorage,
+                              service: service,
+                              synchronizer: KnownCasesSynchronizer(matcher: matcher, service: service, defaults: defaults, descriptor: descriptor),
+                              backgroundTaskManager: backgroundTaskManager,
+                              defaults: defaults)
+        }
     }
 
     func testInitialStatus(){
+        let sdk = SDK()
         let exp = expectation(description: "status")
-        sdk.status { (result) in
+        sdk.sdk.status { (result) in
             switch result {
             case .failure(_):
                 XCTFail()
@@ -88,19 +83,20 @@ class DP3TSDKTests: XCTestCase {
     }
 
     func testCallEnable(){
-        manager.completeActivation()
+        let sdk = SDK()
+        sdk.manager.completeActivation()
         let exp = expectation(description: "enable")
-        try! sdk.startTracing { (err) in
+        try! sdk.sdk.startTracing { (err) in
             exp.fulfill()
         }
         wait(for: [exp], timeout: 2.0)
-        XCTAssertEqual(tracer.state, TrackingState.active)
+        XCTAssertEqual(sdk.tracer.state, TrackingState.active)
     }
 
     func testInfected(){
-
+        let sdk = SDK()
         let stateexp = expectation(description: "stateBefore")
-        sdk.status { (result) in
+        sdk.sdk.status { (result) in
             switch result {
             case .failure(_):
                 XCTFail()
@@ -117,20 +113,20 @@ class DP3TSDKTests: XCTestCase {
         wait(for: [stateexp], timeout: 1.0)
 
         let exp = expectation(description: "infected")
-        keyProvider.keys = [ .init(keyData: Data(count: 16), rollingPeriod: 144, rollingStartNumber: DayDate().period, transmissionRiskLevel: 0, fake: 0) ]
-        sdk.iWasExposed(onset: .init(timeIntervalSinceNow: -.day), authentication: .none) { (result) in
+        sdk.keyProvider.keys = [ .init(keyData: Data(count: 16), rollingPeriod: 144, rollingStartNumber: DayDate().period, transmissionRiskLevel: 0, fake: 0) ]
+        sdk.sdk.iWasExposed(onset: .init(timeIntervalSinceNow: -.day), authentication: .none) { (result) in
             exp.fulfill()
         }
         wait(for: [exp], timeout: 1.0)
-        let model = service.exposeeListModel
+        let model = sdk.service.exposeeListModel
 
-        XCTAssertEqual(outstandingPublishStorage.get().count, 1)
+        XCTAssertEqual(sdk.outstandingPublishStorage.get().count, 1)
 
         if #available(iOS 13.7, *) {
-            XCTAssertFalse(defaults.infectionStatusIsResettable)
+            XCTAssertFalse(sdk.defaults.infectionStatusIsResettable)
         }
         XCTAssert(model != nil)
-        XCTAssertEqual(model!.gaenKeys.count, defaults.parameters.crypto.numberOfKeysToSubmit)
+        XCTAssertEqual(model!.gaenKeys.count, sdk.defaults.parameters.crypto.numberOfKeysToSubmit)
         let rollingStartNumbers = Set(model!.gaenKeys.map(\.rollingStartNumber))
         XCTAssertEqual(rollingStartNumbers.count, model!.gaenKeys.count)
         var runningDate: Date?
@@ -146,7 +142,7 @@ class DP3TSDKTests: XCTestCase {
         }
 
         let stateExpAfter = expectation(description: "stateAfter")
-        sdk.status { (result) in
+        sdk.sdk.status { (result) in
             switch result {
             case .failure(_):
                 XCTFail()
@@ -168,38 +164,41 @@ class DP3TSDKTests: XCTestCase {
         wait(for: [stateExpAfter], timeout: 1.0)
 
 
-        XCTAssertThrowsError(try sdk.startTracing())
+        XCTAssertThrowsError(try sdk.sdk.startTracing())
     }
 
     func testSyncDontCompleteBeforeInit(){
+        let sdk = SDK()
         let exp = expectation(description: "sync")
         exp.isInverted = true
-        sdk.sync(runningInBackground: false) { (result) in
+        sdk.sdk.sync(runningInBackground: false) { (result) in
             exp.fulfill()
         }
         wait(for: [exp], timeout: 1.0)
     }
 
     func testSyncCompleteAfterInit(){
+        let sdk = SDK()
         let exp = expectation(description: "sync")
-        sdk.sync(runningInBackground: false) { (result) in
+        sdk.sdk.sync(runningInBackground: false) { (result) in
             exp.fulfill()
         }
-        manager.completeActivation()
+        sdk.manager.completeActivation()
         wait(for: [exp], timeout: 1.0)
     }
 
     func testSyncWhenActive(){
+        let sdk = SDK()
         let exp = expectation(description: "sync")
-        sdk.sync(runningInBackground: false) { (result) in
+        sdk.sdk.sync(runningInBackground: false) { (result) in
             exp.fulfill()
         }
-        manager.status = .active
+        sdk.manager.status = .active
         MockENManager.authStatus = .authorized
-        manager.isEnabled = true
-        manager.completeActivation()
+        sdk.manager.isEnabled = true
+        sdk.manager.completeActivation()
         wait(for: [exp], timeout: 1.0)
-        XCTAssertEqual(service.requests.count, 10)
+        XCTAssertEqual(sdk.service.requests.count, 10)
     }
 
     struct MockError: Error, Equatable {
@@ -207,12 +206,13 @@ class DP3TSDKTests: XCTestCase {
     }
 
     func testActivationAfterFailure() {
+        let sdk = SDK()
         MockENManager.authStatus = .unknown
         let message = "mockError"
-        manager.completeActivation(error:  MockError(message: message))
+        sdk.manager.completeActivation(error:  MockError(message: message))
         sleep(1)
         let expStatus = expectation(description: "status")
-        sdk.status { (result) in
+        sdk.sdk.status { (result) in
             switch result {
             case let .success(state):
                 switch state.trackingState {
@@ -239,15 +239,15 @@ class DP3TSDKTests: XCTestCase {
 
 
         // app comes again in foreground
-        tracer.willEnterForeground()
+        sdk.tracer.willEnterForeground()
         sleep(1)
 
         MockENManager.authStatus = .authorized
-        manager.completeActivation()
+        sdk.manager.completeActivation()
         sleep(1)
 
         let expStatusAfter = expectation(description: "statusafter")
-        sdk.status { (result) in
+        sdk.sdk.status { (result) in
             switch result {
             case let .success(state):
                 switch state.trackingState {
@@ -265,37 +265,38 @@ class DP3TSDKTests: XCTestCase {
     }
 
     func testEnableAfterEnableFailure(){
-        manager.enableError = MockError(message: "message")
-        manager.isEnabled = true
-        manager.completeActivation()
+        let sdk = SDK()
+        sdk.manager.enableError = MockError(message: "message")
+        sdk.manager.isEnabled = true
+        sdk.manager.completeActivation()
         let exp = expectation(description: "enable")
-        try! sdk.startTracing { (err) in
+        try! sdk.sdk.startTracing { (err) in
             XCTAssert(err != nil)
             XCTAssertEqual((err! as! MockError).message, "message")
             exp.fulfill()
         }
         wait(for: [exp], timeout: 1.0)
-        XCTAssertEqual(tracer.state, TrackingState.inactive(error: .exposureNotificationError(error: manager.enableError!)))
+        XCTAssertEqual(sdk.tracer.state, TrackingState.inactive(error: .exposureNotificationError(error: sdk.manager.enableError!)))
 
-
-        manager.enableError = nil
+        sdk.manager.enableError = nil
 
         // app comes again in foreground
-        tracer.willEnterForeground()
+        sdk.tracer.willEnterForeground()
 
        sleep(1)
 
-        XCTAssertEqual(tracer.state, TrackingState.active)
+        XCTAssertEqual(sdk.tracer.state, TrackingState.active)
     }
 
     func testEnableAfterActivationFailure(){
+        let sdk = SDK()
         MockENManager.authStatus = .unknown
         let error = MockError(message: "mockError")
 
-        manager.completeActivation(error: error)
+        sdk.manager.completeActivation(error: error)
         sleep(1)
         let exp = expectation(description: "enable")
-        try! sdk.startTracing { (err) in
+        try! sdk.sdk.startTracing { (err) in
             XCTAssert(err != nil)
             switch (err! as! DP3TTracingError) {
             case let .exposureNotificationError(error: enError):
@@ -306,18 +307,18 @@ class DP3TSDKTests: XCTestCase {
             exp.fulfill()
         }
         wait(for: [exp], timeout: 1.0)
-        XCTAssertEqual(tracer.state, TrackingState.inactive(error: .exposureNotificationError(error: error)))
+        XCTAssertEqual(sdk.tracer.state, TrackingState.inactive(error: .exposureNotificationError(error: error)))
 
 
         // app comes again in foreground
-        tracer.willEnterForeground()
+        sdk.tracer.willEnterForeground()
 
         sleep(1)
 
-        manager.completeActivation()
+        sdk.manager.completeActivation()
 
         sleep(1)
 
-        XCTAssertEqual(tracer.state, TrackingState.active)
+        XCTAssertEqual(sdk.tracer.state, TrackingState.active)
     }
 }
